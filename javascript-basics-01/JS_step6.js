48ea8ce438d29d4a6a03b378afc4f09b034cd04c
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-07 15:05:51 +0200

    complete 7

--- javascript-basics-01/JS_step6.js
+++ javascript-basics-01/JS_step6.js
@@ -3,6 +3,6 @@ function mult(){
 
     let frst_number = parseInt(document.getElementById("first_number").value);
     let second_number = parseInt(document.getElementById("second_number").value);
-         alert(frst_number /second_number);
+         alert(frst_number %second_number);
     }
     
