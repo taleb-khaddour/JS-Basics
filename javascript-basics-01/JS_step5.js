d1665cf355fe22e9034ed77c9c57e542f7d09e41
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-07 15:02:36 +0200

    complete 6

---
+++ javascript-basics-01/JS_step5.js
@@ -0,0 +1,7 @@
+function mult(){
+
+
+let frst_number = parseInt(document.getElementById("first_number").value);
+let second_number = parseInt(document.getElementById("second_number").value);
+     alert(frst_number *second_number);
+}
---
+++ javascript-basics-01/JS_step6.js
@@ -0,0 +1,8 @@
+function mult(){
+
+
+    let frst_number = parseInt(document.getElementById("first_number").value);
+    let second_number = parseInt(document.getElementById("second_number").value);
+         alert(frst_number /second_number);
+    }
+    
