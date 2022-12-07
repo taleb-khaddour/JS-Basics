3775b8373137c77427641247b5651437eebbdb6a
taleb-khaddour <taleb.khaddour.20311@gmail.com>
2022-12-07 10:41:42 +0200

    complete step 4 update

--- javascript-basics-01/JS_step4.js
+++ javascript-basics-01/JS_step4.js
@@ -1,3 +1,4 @@
 let name =prompt( "what is your name");
 let surname = prompt("what is your last name")
-alert("Hello "+ name + " "+ surname);
+let city =prompt("your city ?")
+alert("name :  "+ name + "\n" +"surname :"+surname +"\n"+"city : "+ city);
